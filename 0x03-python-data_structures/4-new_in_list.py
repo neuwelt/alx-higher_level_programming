d14ed847ce15b3ff8b7febd34f92d8f5be1a892d
neuwelt <ineuwelt@neuwelt.co>
2022-07-06 07:34:10 +0000

    bug fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_list_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
-        return(my_list)
+        return my_list.copy()
     else:
         new = my_list.copy()
         new[idx] = element
